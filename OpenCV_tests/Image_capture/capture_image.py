0b904f84cb1181bbde65fc33bc707ebb4b6f72f2
Justin Alvey <jual6332@colorado.edu>
2018-10-29 15:50:23 -0600

    Fixed capture_image.py location, throwing an error trying to push to pylepton folder

---
+++ OpenCV_tests/Image_capture/capture_image.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2
+from pylepton import Lepton
+
+#with Lepton() as l:
+#    a,_ = l.capture()
+
+# Sensor Data is Read-in as 12-bit, extend to 16 bits without loosing data
+#cv2.normalize(a,a,0,65535, cv2.NORM_MINMAX)
+
+#np.right_shift(a,8,a) # Fit data into 8 bits
+
+#cv2.imwrite("output.png", np.uint8(a))
