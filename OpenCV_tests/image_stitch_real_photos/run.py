b2b708e417787171b7dec8d182e5e3538a4ef242
Jacob Killelea <Jkillelea@protonmail.ch>
2018-11-05 12:13:48 -0700

    pre delete

--- OpenCV_tests/image_stitch_real_photos/run.py
+++ OpenCV_tests/image_stitch_real_photos/run.py
@@ -4,21 +4,21 @@ import cv2
 import math
 import time
 import sys
-from matplotlib import pyplot as plt
+from   matplotlib import pyplot as plt
 
+# Read images from command line if available or use defaults
 images = sys.argv[1:]
 if len(images) < 2:
-    print("Add more args")
-    sys.exit(1)
+    print("Using default images")
+    images = ["IMG_20181029_121916.jpg", "IMG_20181029_121919.jpg"]
 
+# Initialization of the Feature Detector
+orb = cv2.ORB_create()
 
 for image in images:
     ## ORB Implementation - The Algorithm in Action
-    # Read in first image
+    # Read in image
     img = cv2.imread(image, 0)
-
-    # Initialization of the Feature Detector
-    orb = cv2.ORB_create()
 
     # Locate keypoints in the image
     kp = orb.detect(img, None)
@@ -27,6 +27,7 @@ for image in images:
     kp, desc = orb.compute(img, kp)
 
     # Draw the Keypoints in the image
-    img2 = cv2.drawKeypoints(img, kp, None, color = (0, 0, 255), flags = 0)
-    plt.imshow(img2)
+    plt.imshow(
+        cv2.drawKeypoints(img, kp, None, color = (0, 0, 255), flags = 0)
+    )
     plt.show()
