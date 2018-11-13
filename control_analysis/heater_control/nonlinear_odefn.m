a6ee4cc63afb6d7c1846c2eba9cc123d5a4bb1c3
Jacob Killelea <Jkillelea@protonmail.ch>
2018-11-13 09:02:38 -0700

    formatting and documentation

--- control_analysis/heater_control/nonlinear_odefn.m
+++ control_analysis/heater_control/nonlinear_odefn.m
@@ -1,10 +1,23 @@
 function [dT] = nonlinear_odefn(t, y, m, cp, Tsurr, Kc, Kr, control_fn)
+% nonlinear_odefn: Full model of the heater dynamics
+% Arguments:
+%          t:          Timestep, seconds
+%          y:          Ode45's state vector, this only only contains delta T, the
+%                      temperature difference from the point linearized about
+%          m:          Mass of the heater, kg
+%          cp:         Specific heat
+%          Tsurr:      Temperature of the ambient environment
+%          Kc:         Conduction heat loss coeff
+%          Kr:         Radiation heat loss coeff
+%          control_fn: closed loop control function, of the
+%                      form Qdot_in = control_fn(T, t)
     sigma = 1.380649e-23; % J * K−1 boltzmann constant
     T = y(1);
 
-    QdotC = -Kc*(T - Tsurr) / (m*cp); % conduction
-    QdotR = -Kr*sigma*(T^4 - Tsurr^4) / (m*cp); % radiation
-    QdotE = control_fn(T, t) / (m*cp); % control
+    % sum of the heat flows
+    QdotC = -Kc*(T - Tsurr);           % conduction
+    QdotR = -Kr*sigma*(T^4 - Tsurr^4); % radiation
+    QdotE = control_fn(T, t);          % control
 
-    dT = QdotC + QdotR + QdotE;
+    dT = (QdotC + QdotR + QdotE) / (m*cp);
 end
