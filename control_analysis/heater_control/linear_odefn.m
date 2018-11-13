840ec085028029687f6f7d4e744f931cd534c653
Jacob Killelea <Jkillelea@protonmail.ch>
2018-11-12 19:15:09 -0700

    reorganize and document

--- control_analysis/heater_control/linear_odefn.m
+++ control_analysis/heater_control/linear_odefn.m
@@ -1,10 +1,24 @@
-function [dT] = linear_odefn(t, y, m, cp, Tsurr, Kc, Kr, control_fn)
+function [dT] = linear_odefn(t, y, m, cp, T0, Kc, Kr, control_fn)
+% linear_odefn: Linearized model of the heater dynamics
+% Arguments:
+%          t:          Timestep, seconds
+%          y:          Ode45's state vector, this only only contains delta T, the
+%                      temperature difference from the point linearized about
+%          m:          Mass of the heater, kg
+%          cp:         Specific heat
+%          T0:         Temperature which to linearize about
+%          Kc:         Conduction heat loss coeff
+%          Kr:         Radiation heat loss coeff
+%          control_fn: closed loop control function, of the
+%                      form Qdot_in = control_fn(dT, t)
+
     sigma = 1.380649e-23; % J * K−1 boltzmann constant
     dT = y(1);
 
-    QdotC = -Kc*dT             / (m*cp); % conduction
-    QdotR = -(4*Kr*sigma*dT^3) / (m*cp); % radiation
-    QdotE = control_fn(dT, t)  / (m*cp); % control
+    % sum of the heat flows
+    QdotC = -Kc*dT;                % conduction
+    QdotR = -(4*Kr*sigma*T0^3)*dT; % radiation
+    QdotE = control_fn(dT, t);     % control
 
-    dT = QdotC + QdotR + QdotE;
+    dT = (QdotC + QdotR + QdotE) / (m*cp);
 end
