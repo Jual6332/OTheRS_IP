4387132216cac71a51f4990851e3c1b1bc471254
Jacob Killelea <Jkillelea@protonmail.ch>
2018-11-16 16:14:05 -0700

    use new PID class

--- control_analysis/arduino-code/src/main.ino
+++ control_analysis/arduino-code/src/main.ino
@@ -2,6 +2,8 @@
  * Control Loop code to regulate heaters using an arduino
  */
 
+#include "pidcontroller.h"
+
 // Heater physical properties
 const float m  = 0.013; // Kg
 const float cp = 920.0; // J/(Kg K)
@@ -16,21 +18,13 @@ const float Kp = 0.0; // Proportional
 const float Ki = 0.0; // Integral
 const float Kd = 0.0; // Derivative
 
-// Control state variables - Heater 1
-float therm1_last_reading       = 0.0; // Derivative control
-unsigned long therm1_last_milis = 0;   // Derivative control
-float therm1_err                = 0.0; // Integral control
-
-// Control state variables - Heater 2
-float therm2_last_reading       = 0.0; // Derivative control
-unsigned long therm2_last_milis = 0;   // Derivative control
-float therm2_err                = 0.0; // Integral control
-
 #define THERMISTOR_1 A0
 #define THERMISTOR_2 A1
 
-#define HEATER_1 D9
-#define HEATER_2 D11
+#define HEATER_1 9
+#define HEATER_2 11
+
+PIDController heater1(100, 1, 1, 1);
 
 void setup() {
     Serial.begin(9600);
@@ -46,5 +40,6 @@ void loop() {
 
 float thermistor_read_degC(int thermistor_pin) {
     uint16_t adc = analogRead(thermistor_pin);
+    return 0.0; // TODO
 }
 
