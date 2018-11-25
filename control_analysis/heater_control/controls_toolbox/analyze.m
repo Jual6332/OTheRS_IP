8caab6ad386234063586cd682bfeab74a57d4098
Jacob Killelea <Jkillelea@protonmail.ch>
2018-11-24 22:35:53 -0700

    misc, more better gains

--- control_analysis/heater_control/controls_toolbox/analyze.m
+++ control_analysis/heater_control/controls_toolbox/analyze.m
@@ -1,17 +1,18 @@
 % TODO: find correct gains for everything
-clear all; clc;
+clear all; close all; clc;
 
 % Physical properties
 m = 0.013; % kg
 cp = 920;  % J / (kg * K)
 kq = 0.1;  % heat loss (linearized)
 
+% open loop
 plant = tf([0  1], ...
-           [1, kq/(m*cp)])
+           [1, kq/(m*cp)]);
 
 % Control gains
-kp = 100;
-ki = 1e-2;
+kp = 1;
+ki = 5e-3;
 kd = -1e-2;
 
 % Closed loop transfer function
