8c7a085246a3a7f6ed500d20e0910a041bcd9a75
mSVENson31 <micahsvenson@gmail.com>
2019-02-19 13:08:31 -0700

    updated diagonalSlice.py to include all dims

--- Current_Software_Modules/GeometricStitch/diagonalSlice.py
+++ Current_Software_Modules/GeometricStitch/diagonalSlice.py
@@ -12,21 +12,19 @@ from random import randrange
 def write_image(fileName,data):
     cv2.imwrite(str(fileName)+".png",data);
 
-
 def diagImgSlice(toprow,topcol,botrow,botcol): #output: topDiag, botDiag
     # read input image
     image = cv2.imread('Inputs/rgb1.png',cv2.IMREAD_COLOR); #image
 
     #initialize arrays to hold each side of slice
-    topDiag = np.zeros(image.shape[0:2]);
-    botDiag = np.zeros(image.shape[0:2]);
-
+    topDiag = np.zeros(image.shape);
+    botDiag = np.zeros(image.shape);
 
    #define diagonal slice using slope. Simplify to find most accurate diagonal path
     slope = Fraction(botrow-toprow,topcol-botcol);
     rise  = slope.numerator;
     run   = slope.denominator;
-
+    print(rise,run,sep="/")
     #check to make sure imgrabing num cols here
     lenCols = topcol;
     riseCount = 0;
@@ -41,13 +39,12 @@ def diagImgSlice(toprow,topcol,botrow,botcol): #output: topDiag, botDiag
         else:
             riseCount+=1;
 
-        topDiag[i,0:lenCols] = image[i,0:lenCols,0];
-        botDiag[i,lenCols:-1] = image[i,lenCols:-1,0];
+        topDiag[i,0:lenCols] = image[i,0:lenCols,:];
+        botDiag[i,lenCols:-1] = image[i,lenCols:-1,:];
 
     #write output arrays to png files
     cv2.imwrite("Outputs/topDiag.png",topDiag);
     cv2.imwrite("Outputs/botDiag.png",botDiag);
-
 
 # diagImgSlice(topx,topy,botx,boty)
 diagImgSlice(0,160,120,0) #img is 120,160,3
